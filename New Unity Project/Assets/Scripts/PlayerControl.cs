d7e9550074580a6bf292e690568615be2faed10a
Nate Mendoza <nmendoza82rg@aol.com>
2019-05-10 16:19:57 -0700

    2nd
    added a free asset ship set and have a random generatd board maker
    working on the player controls got the ship to move and execute but it just jumps to that location

---
+++ New Unity Project/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+
+    public float moveSpeed;
+
+    public Vector3 currentPos;
+    public Vector3 forwardDestination;
+    public Transform rightDestination;
+    public Transform leftDestination;
+    public float distance;
+    public bool isMoving = false;
+    bool buttonsActive;
+
+    float startTime;
+
+    //so player will click buttons to fill the cammand list, the int will dictate the movement type.
+    public List<int> commandQueue = new List<int>();
+
+    public GameBoard gameBoard;
+
+    private int step = 10;
+
+
+    public enum StatePlayer { dead = 0, alive = 1}
+    public StatePlayer playerState;
+    
+
+    void Start()
+    {
+        gameBoard = FindObjectOfType<GameBoard>();
+        currentPos = this.transform.position;
+        isMoving = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isMoving)
+        {
+            buttonsActive = true;
+        }
+        if (isMoving)
+        {
+            MoveShip();
+        }
+    }
+
+    private void MoveShip()
+    {
+        foreach(int command in commandQueue)
+        {
+
+            currentPos = this.transform.position;
+            if (command == 1)
+                MoveForward();
+            if (command == 2)
+                MoveLeft();
+            if (command == 3)
+                MoveRight();
+
+        }
+
+        isMoving = false;
+        commandQueue.Clear();
+    }
+
+    public void SetStartingPosition(int startingRow, int startingCol)
+    {
+        this.transform.position = new Vector3(transform.position.x + startingRow, transform.position.y, transform.position.z + startingCol);
+        
+    }
+
+    //these are the button seletion methods
+
+    public void QueueForwardCommand()
+    {
+        if(buttonsActive)
+            commandQueue.Add(1);
+    }
+    public void QueueLeftCommand()
+    {
+        if (buttonsActive)
+            commandQueue.Add(2);
+    }
+
+    public void QueueRightCommand()
+    {
+        if (buttonsActive)
+            commandQueue.Add(3);
+    }
+
+
+    public void SetSail()
+    {
+        isMoving = !isMoving;
+    }
+
+
+    //these are the command methods
+    public void MoveForward()
+    {
+        this.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + step);
+
+        //startTime = Time.time;
+        //distance = Vector3.Distance(currentPos, forwardDestination);
+        //float distanceCovered = (Time.time - startTime) * moveSpeed;
+        //float fracJourney = distanceCovered / distance;
+
+        //Debug.Log("Moving forward");
+        
+
+        //forwardDestination = new Vector3(transform.position.x, transform.position.y, transform.position.z + step);
+
+        //transform.position = Vector3.Lerp(currentPos, forwardDestination, fracJourney);
+    }
+
+    public void MoveLeft()
+    {
+        this.transform.position = new Vector3(transform.position.x-step, transform.position.y, transform.position.z);
+        
+        //currentPos = this.transform.position;
+        //startTime = Time.time;
+        //distance = Vector3.Distance(currentPos, forwardDestination);
+        //float distanceCovered = (Time.time - startTime) * moveSpeed;
+        //float fracJourney = distanceCovered / distance;
+
+        //Debug.Log("Moving left");
+
+
+        //forwardDestination = new Vector3(transform.position.x - step, transform.position.y, transform.position.z);
+
+        //transform.position = Vector3.Lerp(currentPos, forwardDestination, fracJourney);
+    }
+
+    public void MoveRight()
+    {
+        this.transform.position = new Vector3(transform.position.x + step, transform.position.y, transform.position.z);
+
+        //currentPos = this.transform.position;
+        //startTime = Time.time;
+        //distance = Vector3.Distance(currentPos, forwardDestination);
+        //float distanceCovered = (Time.time - startTime) * moveSpeed;
+        //float fracJourney = distanceCovered / distance;
+
+        //Debug.Log("Moving right");
+
+
+        //forwardDestination = new Vector3(transform.position.x + step, transform.position.y, transform.position.z);
+
+        //transform.position = Vector3.Lerp(currentPos, forwardDestination, fracJourney);
+    }
+
+}
